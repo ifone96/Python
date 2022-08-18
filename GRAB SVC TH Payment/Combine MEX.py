ada07e0056be6a60ce7cfcfe343f804c6b234640
Senthan <senthan.collectius@gmail.com>
2022-08-19 02:47:33 +0700

    add

--- GRAB SVC TH Payment/Combine MEX.py
+++ GRAB SVC TH Payment/Combine MEX.py
@@ -28,32 +28,14 @@ df_combine = pd.concat(df, axis=0)
 #df_combine2 = df_combine.iloc[:,[0,1,7,11]]
 df_combine = df_combine[['report_date', 'debt_id', 'debtor_id', 'last_payment']]
 df_combine = df_combine.assign(debt_id_text="",
-                               DAX_ID="",
                                Account_Number="",
                                Card_Number="",
                                Description="",
                                Amount="",
                                Amount_Amount_in_LCY="",
                                Effective_transaction_date="",
-                               Transaction_Date_Posting=""
+                               Transaction_Date_Posting="=TODAY()"
                                )
-
-# server = 'localhost\sqlexpress' # for a named instance
-# server = 'myserver,port' # to specify an alternate port
-server = 'collectiusdwhph.database.windows.net'
-database = 'dwh_th_2022'
-username = 'atiwat'
-password = '2a#$dfERat^%'
-connect_database = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD=' + password)
-sql_cmd = """
-    select
-    a.alternis_invoicenumber
-    from stage.alternis_account a
-    where alternis_portfolioidname = 'GRAB SVC TH'
-    """
-df_sql = pd.read_sql(sql_cmd, connect_database)
-
 
 todaysdate_filename = str(
     datetime.datetime.now().strftime("Combine MEX %H%M %y%m%d")) + '.xlsx'
@@ -61,31 +43,41 @@ writer = pd.ExcelWriter(todaysdate_filename)
 print("\n",df_combine, f"{todaysdate_filename }""\n")
 
 df_combine.to_excel(writer, index=False, sheet_name= 'Combine_MEX')
-df_sql.to_excel(writer, index=False, engine='xlsxwriter', sheet_name='SQL_MAP')
 # Get the xlsxwriter workbook and worksheet objects.
 workbook  = writer.book
 worksheet = writer.sheets['Combine_MEX']
-worksheet2 = writer.sheets['SQL_MAP']
+
 
 # Add some cell formats.
 format1 = workbook.add_format({'num_format': '@'})
 format2 = workbook.add_format({'num_format': '0.00'})
+format3 = workbook.add_format({'num_format': 'mm/dd/yyyy'})
 
 # Set the column width and format.
-worksheet.set_column('A:A', 12, format1)
-worksheet.set_column('B:B', 12, format1)
-worksheet.set_column('C:C', 15, format1)
+worksheet.set_column('A:A', 12)
+worksheet.set_column('B:B', 12)
+worksheet.set_column('C:C', 15)
 worksheet.set_column('D:D', 15, format2)
 worksheet.set_column('E:E', 15)
-worksheet.set_column('F:F', 10)
-worksheet.set_column('G:G', 18)
-worksheet.set_column('H:H', 18)
-worksheet.set_column('I:I', 14)
-worksheet.set_column('J:J', 12)
-worksheet.set_column('K:K', 26)
-worksheet.set_column('L:L', 28)
-worksheet.set_column('M:M', 26)
-worksheet2.set_column('A:A', 25)
+worksheet.set_column('F:F', 30)
+worksheet.set_column('G:G', 30)
+worksheet.set_column('H:H', 14)
+worksheet.set_column('I:I', 14, format2)
+worksheet.set_column('J:J', 28, format2)
+worksheet.set_column('K:K', 28, format3)
+worksheet.set_column('L:L', 28, format3)
+worksheet.set_column('M:M', 28, format3)
+
+
+#Formula 
+worksheet.write_dynamic_array_formula('E2:E100', '=B2:B100&""')
+worksheet.write_dynamic_array_formula('F2:F100', '=_xlfn.XLOOKUP(B2:B100,Maping_MEX.xlsx!$A:$A,Maping_MEX.xlsx!$B:$B)')
+worksheet.write_dynamic_array_formula('G2:G100', '=_xlfn.XLOOKUP(B2:B100,Maping_MEX.xlsx!$A:$A,Maping_MEX.xlsx!$B:$B)')
+worksheet.write_dynamic_array_formula('I2:I100', '=D2:D100*1')
+worksheet.write_dynamic_array_formula('J2:J100', '=D2:D100*1')
+worksheet.write_dynamic_array_formula('K2:K100', '=_xlfn.DATE(_xlfn.RIGHT(A2:A100,4),_xlfn.MID(A2:A100,4,2),_xlfn.LEFT(A2:A100,2))')
+
+
 
 # Close the Pandas Excel writer and output the Excel file.
 writer.save()
@@ -108,6 +100,6 @@ path_file = path_url + "\*.xls*"
 for filex in glob.iglob(path_file, recursive=True):
     os.path.realpath(path_url)
     # FBI OPEN UP!!!!
-    os.startfile(path_url)
+   #os.startfile(path_url)
     os.startfile(filex)
-    print('Opened File&Folder:', filex)
+    print('Opened File&Folder:', filex)    
